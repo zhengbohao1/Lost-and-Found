cdd764568fbdf4f8f0f095a86276816772a1381d
zhishixb <1957961165@qq.com>
2024-09-16 16:49:12 +0800

    对错误的修改

--- src/main/resources/Lost_and_Found/src/views/HomePage.vue
+++ src/main/resources/Lost_and_Found/src/views/HomePage.vue
@@ -1,21 +1,38 @@
 <template>  
-    <el-container style="height: 80vh;">  
-      <el-header>
-        <el-row>
-        <el-col :span="3">
-           <el-image class="component" :src="require('@/assets/logo_longer.png')" style="height: 80%;"/>
-        </el-col>
-      </el-row>
-      </el-header>  
-        <el-divider></el-divider>
-        <el-aside width="200px">Aside</el-aside>  
-          
-        <el-main>Main Content</el-main>  
-      </el-container>   
-  </template>  
-    
-  <script setup>  
-  </script>  
-    
-  <style scoped>  
-  </style>
+  <el-container style="height: 80vh;">  
+    <el-header>
+      <el-row>
+      <el-col :span="3">
+         <el-image class="component" :src="require('@/assets/logo_longer.png')" style="width: 100%;"/>
+      </el-col>
+      <el-col :span="18">
+        <el-menu  
+            style="margin: 0; padding: 0; height: 100%;"
+            mode="horizontal"  
+          >  
+          <el-menu-item index="1" style="width:23%; font-size: 20px;">首页</el-menu-item>
+          <el-menu-item index="1" style="width:23%; font-size: 20px;">关于我们</el-menu-item>
+          <el-menu-item index="1" style="width:23%; font-size: 20px;">联系我们</el-menu-item>
+          <el-menu-item index="1" style="width:23%; font-size: 20px;">加入我们</el-menu-item>
+        </el-menu>
+      </el-col>
+      <el-col :span="3">
+        <el-button type="primary" round style="height: 60%; width: 60%;font-size: 20px;" @click="login">登录</el-button>
+      </el-col>
+    </el-row>
+    </el-header>      
+      <el-main>Main Content</el-main>  
+    </el-container>   
+</template>  
+  
+<script setup>
+import {useRouter} from 'vue-router'
+
+const router = useRouter();
+const login = () => {
+  router.push('/login');
+} //跳转至登录页面
+</script>  
+  
+<style scoped>  
+</style>
